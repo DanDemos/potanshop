9ddcae66d8556c00e48a63f92b4ed9541747f52e
DanDemos <dandemo14613@gmail.com>
2025-08-04 20:27:09 +0700

    game detail carousal fixed

--- src/components/GameDetailCarousel.tsx
+++ src/components/GameDetailCarousel.tsx
@@ -25,30 +25,49 @@ export function GameDetailCarousel(): JSX.Element {
         <div className="w-full sm:w-2/3 h-full">
           {" "}
           {/* Carousel takes 2/3 of the screen width */}
-          <Slider {...settings}>
-            {gameDetail?.banner?.map((eachBanner, key) => {
-  console.log(gameDetail?.description, "eachBanner")
-
-              return (
-                <div key={key} className="relative w-full h-full">
-                  <img
-                    src={eachBanner?.banner_image}
-                    alt="Banner 1"
-                    className="object-cover w-full h-full"
-                  />
-                  <div className="absolute inset-0 bg-black opacity-30"></div>
-                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-                    <h2 className="text-4xl font-bold">
-                      {eachBanner?.banner_title}
-                    </h2>
-                    <p className="mt-4 text-xl">
-                      {eachBanner?.banner_description}
-                    </p>
-                  </div>
+          {gameDetail?.banner?.length > 0 ? (
+            gameDetail?.banner?.length == 1 ? (
+              <div className="relative w-full h-full">
+                <img
+                  src={gameDetail?.banner?.[0]?.banner_image}
+                  alt="Banner 1"
+                  className="object-cover w-full h-full"
+                />
+                <div className="absolute inset-0 bg-black opacity-30"></div>
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+                  <h2 className="text-4xl font-bold">
+                    {gameDetail?.banner?.[0]?.banner_title}
+                  </h2>
+                  <p className="mt-4 text-xl">
+                    {gameDetail?.banner?.[0]?.banner_description}
+                  </p>
                 </div>
-              );
-            })}
-            {/* 
+              </div>
+            ) : (
+              <Slider {...settings}>
+                {gameDetail?.banner?.map((eachBanner, key) => {
+                  console.log(gameDetail?.description, "eachBanner");
+
+                  return (
+                    <div key={key} className="relative w-full h-full">
+                      <img
+                        src={eachBanner?.banner_image}
+                        alt="Banner 1"
+                        className="object-cover w-full h-full"
+                      />
+                      <div className="absolute inset-0 bg-black opacity-30"></div>
+                      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+                        <h2 className="text-4xl font-bold">
+                          {eachBanner?.banner_title}
+                        </h2>
+                        <p className="mt-4 text-xl">
+                          {eachBanner?.banner_description}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+                {/* 
             <div className="relative w-full h-full">
               <img
                 src={
@@ -80,16 +99,21 @@ export function GameDetailCarousel(): JSX.Element {
                 <p className="mt-4 text-xl">Fresh products just for you</p>
               </div>
             </div> */}
-          </Slider>
+              </Slider>
+            )
+          ) : null}
         </div>
 
-        <div className="hidden sm:block sm:w-1/3 px-8 pt-7">
+        <div className="hidden sm:block sm:w-1/3 px-8 py-7">
           {" "}
           {/* Right side for the paragraph */}
           <h3 className="text-md font-bold mb-2">
             {gameDetail?.data?.game_title}
           </h3>
-          <div className="line-clamp-[17] overflow-hidden text-ellipsis" dangerouslySetInnerHTML={{ __html: gameDetail?.description }} />
+          <div
+            className="line-clamp-[17] overflow-hidden text-ellipsis"
+            dangerouslySetInnerHTML={{ __html: gameDetail?.description }}
+          />
         </div>
       </div>
     </div>
